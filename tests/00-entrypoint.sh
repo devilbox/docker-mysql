0d35e7219a405c974f53e2f1eb111da87abba6e5
cytopia <cytopia@everythingcli.org>
2020-11-13 14:13:22 +0100

    Fix entrypoint.sh

--- tests/00-entrypoint.sh
+++ tests/00-entrypoint.sh
@@ -9,6 +9,9 @@ VERSION="${2}"
 
 
 # Ensure they all have a common entrypoint
-docker image inspect "${IMAGE}:${TYPE}-${VERSION}" \
+if ! docker image inspect "${IMAGE}:${TYPE}-${VERSION}" \
 	| grep '"Entrypoint":' -A 1 \
-	| grep '"docker-entrypoint.sh"'
+	| grep -E '"/?docker-entrypoint.sh"'; then
+	docker image inspect "${IMAGE}:${TYPE}-${VERSION}" | grep -i "entrypoint" || true
+	exit 1
+fi
